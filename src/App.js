c1a66b969df7dd8902ec3c666fba613f64538348
aung <aung@umich.edu>
2019-07-14 20:30:59 -0700

    Avatar testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "./App.css";
+import Modal from 'react-awesome-modal';
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       success : false,
-      url : ""
+      url : "",
+      visible: false
     }
   }
+
+  openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
 
   handleChange = (ev) => {
     this.setState({success: false, url : ""});
@@ -69,9 +85,52 @@ class App extends Component {
     )
     return (
       <div className="App">
+      {/*
+      <section>
+              <h1>React-Modal Examples</h1>
+              <input type="button" value="Open" onClick={() => this.openModal()} />
+              <Modal
+                  visible={this.state.visible}
+                  width="400"
+                  height="300"
+                  effect="fadeInUp"
+                  onClickAway={() => this.closeModal()}
+              >
+                  <div>
+                      <h1>Title</h1>
+                      <p>Some Contents</p>
+                      <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                  </div>
+              </Modal>
+          </section>*/}
+          <Modal
+              visible={this.state.visible}
+              width="400"
+              height="300"
+              effect="fadeInUp"
+              onClickAway={() => this.closeModal()}
+          >
+              <div className='modalContentCenter'>
+                  <div id='firstModalLine'>
+                  <button className='font-weight-bold'>View picture</button>
+                  </div>
+                  <hr style={{'vertical-align': 'middle'}} />
+                  <div id='secondModalLine'>
+                  <button className='font-weight-bold mb-3'>Change picture</button>
+                  <input id='hiddenInput' onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
+                  </div>
+                  <br />
+                  <a href="javascript:void(0);" onClick={() => this.closeModal()} id="closeModalButton">Close</a>
+              </div>
+          </Modal>
         <center>
           <h1>UPLOAD A FILE</h1>
           {this.state.success ? <Success_message/> : null}
+          <div className="container">
+            <img className="avatar" src={this.state.url ? this.state.url : "https://bbprofileimage.s3.amazonaws.com/mypic"} />
+            <button className="btn" value="Open" onClick={() => this.openModal()}></button>
+          </div>
+
           <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
           <br/>
           <button onClick={this.handleUpload}>UPLOAD</button>
